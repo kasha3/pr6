8a814095571ca2c8ffd89949fb4c3479eb3db9fe
FadAlex <fadeevsasha0906@gmail.com>
2024-12-02 22:21:24 +0500

    Разработанный класс User. Фадеев.

--- Classes/SendMail.cs
+++ Classes/SendMail.cs
@@ -11,10 +11,10 @@ namespace RegIN_Fadeev.Classes
             var smtpClient = new SmtpClient("smtp.yandex.ru")
             {
                 Port = 587,
-                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                Credentials = new NetworkCredential("yandex@yandex.ru", "blhlbievoaadxhef"),
                 EnableSsl = true,
             };
-            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn" ,Message);
+            smtpClient.Send("reginpr6@yandex.ru", To, "Проект RegIn" ,Message);
         }
     }
 }
